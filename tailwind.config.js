04bb055ac0bbb4129a1a39ef85b6c400c2c897a6
valeriy <tyryskin94@gmail.com>
2024-02-19 14:37:05 +0200

    project added to github, development started

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/forms')],
+}
