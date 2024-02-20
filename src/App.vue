42f2259e5c0e18bbe25b90c2d57928fff6e7c5ed
valeriy <tyryskin94@gmail.com>
2024-02-20 13:41:29 +0200

    added page+filter+saving

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,40 @@ export default {
       ticker: '',
       tickers: [],
     sel: null,
-    graph: []
+    graph: [],
+    page: 1,
+    filter: '',
+    hasNextpage: true,
+    }
+  },
+
+  watch: {
+    filter() {
+      this.page = 1
+      window.history.pushState(
+        null, 
+        document.title, 
+        `${window.location.pathname}?filter=${this.filter}&page=${this.page}`
+      )
+    },
+    page() {
+      window.history.pushState(
+        null, 
+        document.title, 
+        `${window.location.pathname}?filter=${this.filter}&page=${this.page}`
+      )
     }
   },
 
   created() {
+    const windowData = Object.fromEntries(new URL(window.location).searchParams.entries())
+    if (windowData.filter) {
+      this.filter = windowData.filter
+    }
+
+    if (windowData.page) {
+      this.page = windowData.page
+    }
     const tickersData = localStorage.getItem('cryptonomicon-list')
     if (tickersData) {
       this.tickers = JSON.parse(tickersData)
@@ -20,6 +49,17 @@ export default {
     }
   },
   methods: {
+    filteredTickers() {
+      const start = (this.page - 1) * 6
+      const end = this.page*6
+
+     const filteredTickers = this.tickers
+        .filter(ticker => ticker.name
+        .includes(this.filter))
+
+        this.hasNextpage = filteredTickers.length > end
+        return filteredTickers.slice(start, end)
+    },
     subscribeToUpdate(tickerName) {
       setInterval(async () => {
         const f = await fetch(`
@@ -40,6 +80,7 @@ export default {
         price: '-' 
       };
       this.tickers.push(currentTicker)
+      this.filters = ''
 
       localStorage.setItem('cryptonomicon-list', JSON.stringify(this.tickers))
       this.subscribeToUpdate(currentTicker.name)
@@ -107,9 +148,26 @@ export default {
       </button>
     </section>
       <template v-if="tickers.length">
+        <div>
+          <hr class="w-full border-t border-gray-600 my-4" />
+          <button 
+            v-if="page > 1"
+            @click="page = page - 1"
+            class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
+            Назад
+          </button>
+          <button 
+            v-if="hasNextpage"
+            @click="page = page + 1"
+            class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
+            Вперед
+          </button>
+          <div>Фильтр: <input v-model="filter"/></div>
+          <hr class="w-full border-t border-gray-600 my-4" />
+        </div>
       <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
         <div
-          v-for="t in tickers"
+          v-for="t in filteredTickers()"
           :key="t.name"
           @click="select(t)"
           :class="{
