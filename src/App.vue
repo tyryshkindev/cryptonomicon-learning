c075f123d4a79de030b29fd0504871537753bac2
valeriy <tyryskin94@gmail.com>
2024-02-20 16:47:48 +0200

    added validation(existing tickers)

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@ export default {
     page: 1,
     filter: '',
     hasNextpage: true,
+    exists: false,
     }
   },
 
@@ -73,17 +74,25 @@ export default {
         }
       }, 5000);
       this.ticker = ""
-    },  
+    },
+    resetExists() {
+      this.exists = false
+    },
     add() {
-      const currentTicker = { 
-        name: this.ticker, 
-        price: '-' 
-      };
-      this.tickers.push(currentTicker)
-      this.filters = ''
+        const currentTicker = { 
+          name: this.ticker.toUpperCase(), 
+          price: '-' 
+        };
+        if (!(this.tickers.some(t => t.name === currentTicker.name))) {
+          this.tickers.push(currentTicker)
+          this.exists = false
+          this.filters = ''
 
-      localStorage.setItem('cryptonomicon-list', JSON.stringify(this.tickers))
-      this.subscribeToUpdate(currentTicker.name)
+          localStorage.setItem('cryptonomicon-list', JSON.stringify(this.tickers))
+          this.subscribeToUpdate(currentTicker.name)
+        } else {
+          this.exists = true
+        }
     },
     handleDelete(tickerToRemove) {
       this.tickers = this.tickers.filter(t => t !== tickerToRemove)
@@ -105,6 +114,12 @@ export default {
 
 <template>
 <div class="container mx-auto flex flex-col items-center bg-gray-100 p-4">
+  <!-- <div class="fixed w-100 h-100 opacity-80 bg-purple-800 inset-0 z-50 flex items-center justify-center">
+    <svg class="animate-spin -ml-1 mr-3 h-12 w-12 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+      <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+      <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+    </svg>
+  </div> -->
   <div class="container">
     <div class="w-full my-4"></div>
     <section>
@@ -117,6 +132,7 @@ export default {
             <input
               v-model="ticker"
               @keydown.enter="add"
+              @input="resetExists"
               type="text"
               name="wallet"
               id="wallet"
@@ -124,6 +140,22 @@ export default {
               placeholder="Например DOGE"
             />
           </div>
+          <div class="flex bg-white p-1 rounded-md shadow-md flex-wrap">
+            <span class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
+              BTC
+            </span>
+            <span class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
+              DOGE
+            </span>
+            <span class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
+              BCH
+            </span>
+            <span class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
+              CHD
+            </span>
+          </div>
+          <div v-if="exists" class="text-sm text-red-600">Такой тикер уже добавлен</div>
+
         </div>
       </div>
       <button
